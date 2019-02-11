9734a06db48b0255dbd112e233a0aec9a8f97062
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-02-11 09:07:10 -0500

    progress

--- index.js
+++ index.js
@@ -12,53 +12,22 @@ class Neighborhood {
     this.name = name;
     this.id = neighborhoodId++;
     store.neighborhoods.push(this);
+  } 
+  deliveries() {
+    return store.deliveries.filter(delivery => {
+      return delivery.neighborhoodId == this.id;
+    });
   }
-//   trips() {
-//     return store.trips.filter(trip => {
-//       return trip.driverId == this.id;
-//     });
-//   }
-//   passengers() {
-//     return this.trips().map(trip => {
-//       return trip.passenger();
-//     });
-//   }
+  customers() {
+    return store.customers.filter(customer => {
+      return customer.deliveryId == this.deliveryId;
+    });
+  }
+  meals() {
+    return this.meals().flter(meal => {
+
+    });
+  }
 }
 
-// class Passenger {
-//   constructor(name) {
-//     this.name = name;
-//     this.id = passengerId++;
-//     store.passengers.push(this);
-//   }
 
-//   trips() {
-//     return store.trips.filter(trip => {
-//       return trip.passengerId == this.id;
-//     });
-//   }
-//   drivers() {
-//     return this.trips().map(trip => {
-//       return trip.driver();
-//     });
-//   }
-// }
-
-// class Trip {
-//   constructor(driver, passenger) {
-//     this.driverId = driver.id;
-//     this.passengerId = passenger.id;
-//     this.id = tripId++;
-//     store.trips.push(this);
-//   }
-//   driver() {
-//     return store.drivers.find(driver => {
-//       return driver.id === this.driverId;
-//     });
-//   }
-//   passenger() {
-//     return store.passengers.find(passenger => {
-//       return passenger.id === this.passengerId;
-//     });
-//   } 
-// }
